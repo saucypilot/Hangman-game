fb0b6768b953fedf8ed1828630de43258bdae7c4
Irian <durianirian@gmail.com>
2023-01-22 00:51:08 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 
 using namespace std;
 
-void gameUI(){
+void gameUI()
+{
     cout << "  +---+" << endl;
     cout << "  |   |" << endl;
     cout << "  O   |" << endl;
@@ -13,12 +14,42 @@ void gameUI(){
     cout << "=========" << endl;
 }
 
-string usersWord{
+// Gets hidden word/phrase from user
+string usersWord
+{
     string word;
     cout << "For the game, have one chosen player make the hidden word or phrase. The other player will guess the word." << endl;
-    cout << "Chosen player, What is your secret phrase or word? ";
+    cout << "Chosen player, What is your secret phrase or word? Press enter when done." << endl;
     cin >> word;
     return word;
+}
+
+// Turns user input into hidden dashes
+string hiddenWord(string word)
+{
+    string hiddenWord;
+    for (int i = 0; i < word.length(); i++)
+    {
+        hiddenWord += "_";
+    }
+    return hiddenWord;
+}
+
+// Gets user guess
+string userGuess()
+{
+    cout << "It's time to guess the hidden word or phrase. You have 6 guesses. Good luck!" << endl;
+    string guess;
+    return guess;
+}
+
+// Checks if guess is correct
+bool checkGuess(string guess, string word)
+{
+    for (int i = 0; i < word.length(); i++)
+    {
+        
+    }
 }
 
 int main()
