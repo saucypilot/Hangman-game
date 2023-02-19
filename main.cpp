19067ea2bfeadd041c93005066d3af835e353af7
Irian Durian <durianirian@gmail.com>
2023-02-19 13:13:43 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,13 +3,24 @@
 
 using namespace std;
 
-string chosenPassword();
-string hidePassword(string word);
+string chosenPassword()
+{
+    string chosenPassword;
+    cout << "Choose the player that will choose the password for all of you to guess and have him enter the password below:" << endl;
+    cin >> chosenPassword;
+};
+
+string hidePassword(string word)
+{
+    string hiddenPassword(chosenPassword.length(), '*');
+};
+
 string displayGameUI();
+
 bool checkLetter();
 
 int main()
 {
-    cout<<"Welcome to Hangman!"<<endl;
+    cout << "Welcome to Hangman!" << endl;
     return 0;
 }
